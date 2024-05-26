12e544a76f04482e6478e7463a5178c9d6db0b8a
Daffadon <daffaputranarendra9@gmail.com>
2024-05-27 00:48:26 +0700

    feat: add register-controller

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,5 +1,16 @@
+import { registerUser } from '../service/user/register';
 import loginService from '../service/user/login-service';
+import { MiddlewareRequest } from '../interface/controller-interface';
 import { MiddlewareFunction } from '../interface/controller-interface';
+
+const register = async ({ req, res, next }: MiddlewareRequest) => {
+  try {
+    const result = await registerUser(req?.body);
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
 const loginController = async ({ req, res, next }: MiddlewareFunction) => {
   try {
@@ -12,4 +23,4 @@ const loginController = async ({ req, res, next }: MiddlewareFunction) => {
   }
 };
 
-export default { loginController };
+export { register, loginController };
--- src/routes/public-api.ts
+++ src/routes/public-api.ts
@@ -1,11 +1,10 @@
 import express from 'express';
-import { register } from '../controller/user-controller';
-import userController from '../controller/user-controller';
+import { register, loginController } from '../controller/user-controller';
 
 const publicRouter = express.Router();
 
 publicRouter.post('/register', (req, res, next) => register({ req, res, next }));
 
-publicRouter.post('/login', (req, res, next) => userController.loginController({ req, res, next }));
+publicRouter.post('/login', (req, res, next) => loginController({ req, res, next }));
 
 export { publicRouter };
