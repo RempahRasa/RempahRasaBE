fe5a212580b7af7585df6d952b8cc23931057b03
Daffadon <daffaputranarendra9@gmail.com>
2024-05-27 00:47:58 +0700

    feat: add register endpoint

--- src/routes/public-api.ts
+++ src/routes/public-api.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import { register } from '../controller/user-controller';
 import userController from '../controller/user-controller';
 
 const publicRouter = express.Router();
+
+publicRouter.post('/register', (req, res, next) => register({ req, res, next }));
 
 publicRouter.post('/login', (req, res, next) => userController.loginController({ req, res, next }));
 
