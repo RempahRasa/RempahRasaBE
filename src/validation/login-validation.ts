08813f1438bdba5dee03500556dd58238970582b
Reqyan <reqyan.1103@gmail.com>
2024-05-27 16:44:25 +0700

    chore: add bycrypt

--- src/validation/login-validation.ts
+++
@@ -1,8 +0,0 @@
-import z from 'zod'
-
-const loginUserValidation = z.object({
-    email: z.string().email({ message: "Invalid email address" }).max(100),
-    password: z.string().max(100)
-})
-
-export { loginUserValidation }
