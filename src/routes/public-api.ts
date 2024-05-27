510a150b5d406f3104098da9d476018009fd3e67
Daffadon <daffaputranarendra9@gmail.com>
2024-05-27 20:09:46 +0700

    feat: add multer to handle image image in request

--- src/routes/public-api.ts
+++ src/routes/public-api.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import { register, loginController } from '../controller/user-controller';
 
+import multer from 'multer';
+import { MulterRequest } from '../interface/request-interface';
+const upload = multer();
 const publicRouter = express.Router();
 
-publicRouter.post('/register', (req, res, next) => register({ req, res, next }));
-
 publicRouter.post('/login', (req, res, next) => loginController({ req, res, next }));
+publicRouter.post('/register', upload.single('image'), (req: MulterRequest, res, next) => {
+  register({ req, res, next });
+});
 
 export { publicRouter };
