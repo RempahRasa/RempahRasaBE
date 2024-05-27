5e3b92472663470ec46e5b265d8fe373319f03d4
Daffadon <daffaputranarendra9@gmail.com>
2024-05-27 16:15:28 +0700

    style: change return parsing error message

--- src/validation/user/register-user-validation.ts
+++ src/validation/user/register-user-validation.ts
@@ -3,8 +3,8 @@ const FILESIZE = 5000000;
 const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'];
 
 const registerUserValidation = z.object({
-  firstName: z.string().min(1, 'FirstName is required'),
-  lastName: z.string().min(1, 'Lastname is required'),
+  firstName: z.string().min(1, 'First name is required'),
+  lastName: z.string().min(1, 'Last name is required'),
   email: z.string().email().min(1, 'Email is required'),
   password: z.string().min(8, 'Password must be at least 8 characters'),
   image: z.string().optional()
