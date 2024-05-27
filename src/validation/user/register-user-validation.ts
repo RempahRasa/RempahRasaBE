3bacbb174d14677b8a29de80048825b523272e55
Daffadon <daffaputranarendra9@gmail.com>
2024-05-27 20:31:37 +0700

    feat: change image validation to file image

--- src/validation/user/register-user-validation.ts
+++ src/validation/user/register-user-validation.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+
 const FILESIZE = 5000000;
 const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'];
 
@@ -7,16 +8,15 @@ const registerUserValidation = z.object({
   lastName: z.string().min(1, 'Last name is required'),
   email: z.string().email().min(1, 'Email is required'),
   password: z.string().min(8, 'Password must be at least 8 characters'),
-  image: z.string().optional()
-  // image: z
-  //   .any()
-  //   .optional()
-  //   .refine((file) => {
-  //     file?.size < FILESIZE, 'file size too large';
-  //   })
-  //   .refine((file) => {
-  //     ACCEPTED_IMAGE_TYPES.includes(file?.[0].type), 'only jpeg, jpg, png, webp allowed';
-  //   })
+  image: z
+    .any()
+    .optional()
+    .refine((file) => {
+      return file.size < FILESIZE, 'file size too large';
+    })
+    .refine((file) => {
+      return ACCEPTED_IMAGE_TYPES.includes(file.mimetype), 'only jpeg, jpg, png, webp allowed';
+    })
 });
 
 export { registerUserValidation };
