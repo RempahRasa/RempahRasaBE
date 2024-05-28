1c265dbb945481ccd1e2d2def45eb837185d02ea
Reqyan <reqyan.1103@gmail.com>
2024-05-29 04:50:47 +0700

    fix: password overwrite in firestore

--- src/service/user/login-service.ts
+++ src/service/user/login-service.ts
@@ -8,6 +8,7 @@ import { db } from '../../app/firestore';
 
 const loginService = async (request: requestLoginInterface) => {
   const validateUser = validate(loginUserValidation, request);
+  
   if (validateUser) {
     const userCollection = db.collection('users');
     const user = await userCollection.where('email', '==', validateUser.email).get();
@@ -20,15 +21,25 @@ const loginService = async (request: requestLoginInterface) => {
     }
     const userDoc = user.docs[0];
     const userData = userDoc.data();
-    const hashedPassword = await bcrypt.hash(validateUser.password, 10);
-
-    if (!bcrypt.compare(hashedPassword, userData.password)) {
+    const isPasswordValid = await bcrypt.compare(validateUser.password, userData.password);
+    if (isPasswordValid === false) {
       throw new ResponseError(401, 'Username or Password is wrong');
     }
+    if (!userData.verified) {
+      throw new ResponseError(401, 'Please verify your email');
+    }
     const accessToken = generateToken();
-    const res = await userCollection.doc(userDoc.id).update({ accessToken });
-    console.log(accessToken);
-    // return res;
+    const accessTokenExpires = new Date().setHours(new Date().getHours() + 2);
+    const newUser = {
+      accessToken,
+      accessTokenExpires
+    }
+    const userLogin = await userCollection.doc(userDoc.id).update(newUser);
+    if (userLogin) {
+      return {
+        accessToken
+      };
+    } 
   }
 };
 
