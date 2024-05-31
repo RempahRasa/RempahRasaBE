5dcf012217921e89470c3a383c1cdf8a02f6d26b
Daffadon <daffaputranarendra9@gmail.com>
2024-05-31 22:23:23 +0700

    feat: add token controller

--- src/controller/token-controller.ts
+++ src/controller/token-controller.ts
@@ -1,16 +1,28 @@
-import { MiddlewareRequest } from "../interface/controller-interface";
-import { verificationService } from "../service/token/verification";
+import { verificationService } from '../service/token/verification';
+import { MiddlewareRequest } from '../interface/controller';
+import { resendTokenByEmail } from '../service/token/resend-token';
 
-const tokenController = async ({req, res, next}:MiddlewareRequest )  => { 
-    try {
-        const token = req.query.token;
-        const result = await verificationService(token as string);
-        res.status(200).json({
-            data: result
-        });
-    } catch (error) {
-        next(error);
-    }
-}
+const tokenController = async ({ req, res, next }: MiddlewareRequest) => {
+  try {
+    const token = req.query.token;
+    const result = await verificationService(token as string);
+    res.status(200).json({
+      data: result
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-export {tokenController};
+const resendToken = async ({ req, res, next }: MiddlewareRequest) => {
+  try {
+    const result = await resendTokenByEmail(req?.body.email);
+    res.status(200).json({
+      data: result
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { resendToken, tokenController };
