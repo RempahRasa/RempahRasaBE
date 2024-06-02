6b74c1efc6c057c023385bffb94cb0510216cf6c
Daffadon <daffaputranarendra9@gmail.com>
2024-06-02 20:26:06 +0700

    feat: add register and logout services

---
+++ src/service/user/logout-service.ts
@@ -0,0 +1,19 @@
+import { Request } from 'express';
+import { db } from '../../app/firestore';
+import { getUserByToken } from '../../utils/getUserByToken';
+import { FieldValue } from '@google-cloud/firestore';
+
+const logout = async (request: Request) => {
+  const bearerToken = request.get('Authorization');
+  const accessToken = bearerToken.split(' ')[1];
+  const { user, result } = await getUserByToken(accessToken);
+  const userCollections = db.collection('users');
+  await userCollections.doc(user.docs[0].id).update({
+    accessTokenExpires: FieldValue.delete(),
+    accessToken: FieldValue.delete()
+  });
+  return {
+    message: 'Logout success'
+  };
+};
+export { logout };
---
+++ src/service/user/register-service.ts
@@ -0,0 +1,41 @@
+import { ResponseError } from '../../error/response-error';
+import { RequestSignupInterface } from '../../interface/request';
+import { generateToken } from '../../utils/token';
+import { registerUserValidation } from '../../validation/user/register-user-validation';
+import { validate } from '../../validation/validation';
+import bcrypt from 'bcryptjs';
+import { getUserByEmail } from '../../utils/getUserByEmail';
+import { db } from '../../app/firestore';
+
+const registerUser = async (request: RequestSignupInterface) => {
+  const validatedUser = validate(registerUserValidation, request);
+  if (validatedUser) {
+    const { result} = await getUserByEmail(validatedUser.email);
+    if (result.length > 0) {
+      throw new ResponseError(409, 'Email already registered');
+    }
+    validatedUser.password = await bcrypt.hash(validatedUser.password, 10);
+    const verificationToken = generateToken();
+    const verificationTokenExpires = new Date().setMinutes(new Date().getMinutes() + 10);
+    const newUser = {
+      ...validatedUser,
+      image: '',
+      verificationToken,
+      verificationTokenExpires,
+      verified: false
+    };
+
+    const userCollections = db.collection('users');
+    const userRegistered = await userCollections.add(newUser);
+    if (userRegistered) {
+      return {
+        id: userRegistered.id,
+        verificationToken,
+        email: newUser.email,
+        massage: 'User registered successfully'
+      };
+    }
+  }
+};
+
+export { registerUser };
