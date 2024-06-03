8e0f303c266ba3b98c3f97ac32dd350ded269545
Daffadon <daffaputranarendra9@gmail.com>
2024-06-03 19:42:03 +0700

    feat: create getTokenFromHeader

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,5 +1,22 @@
 import * as cr from 'crypto';
+import { Request, Response } from 'express';
 const generateToken = () => {
-  return cr.randomBytes(32).toString('hex');
+  return cr.randomBytes(64).toString('hex');
 };
-export { generateToken };
+
+const getTokenFromHeader = (req: Request, res: Response) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res
+      .status(401)
+      .json({
+        errors: 'Unauthorized'
+      })
+      .end();
+    return;
+  }
+  const token = authHeader.split(' ')[1];
+  return token;
+};
+
+export { generateToken, getTokenFromHeader };
