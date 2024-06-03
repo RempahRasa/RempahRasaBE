773d4b00449c119d29c48f05c85a2f11dc6d5a49
Daffadon <daffaputranarendra9@gmail.com>
2024-06-03 19:16:30 +0700

    refractor: change return value

--- src/utils/user/getUserByEmail.ts
+++ src/utils/user/getUserByEmail.ts
@@ -1,13 +1,13 @@
-import { db } from '../app/firestore';
+import { userCollection } from '../../app/firestore';
 
 const getUserByEmail = async (email: string) => {
-  const userCollection = db.collection('users');
   const user = await userCollection.where('email', '==', email).get();
   const result = user.docs.map((doc) => {
     const data = doc.data();
     return data;
   });
-  return { user, result };
+  const userData = result[0];
+  return { userData };
 };
 
 export { getUserByEmail };
