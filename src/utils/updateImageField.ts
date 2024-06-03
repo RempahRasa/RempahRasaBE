0a1590cbd81ee4d97bbac9cfa8824d49e5e0869f
Daffadon <daffaputranarendra9@gmail.com>
2024-06-03 19:42:29 +0700

    refractor: change using userCollection instance

--- src/utils/updateImageField.ts
+++ src/utils/updateImageField.ts
@@ -1,9 +1,8 @@
-import { db } from '../app/firestore';
+import { userCollection } from '../app/firestore';
 
 const updateImageField = async (imageURl: string, id: string) => {
-  const userCollections = db.collection('users');
   try {
-    await userCollections.doc(id).update({ image: imageURl });
+    await userCollection.doc(id).update({ image: imageURl });
     return true;
   } catch (error) {
     return false;
