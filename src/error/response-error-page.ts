7ba626bfb21800853a268718fe8c2ce3f03a9619
Daffadon <daffaputranarendra9@gmail.com>
2024-06-03 19:33:20 +0700

    refractor: remove unused code

--- src/error/response-error-page.ts
+++ src/error/response-error-page.ts
@@ -1,14 +1,11 @@
-import path from "path";
-
 class VerificationResponseError extends Error {
-    status: number;
-    pathFile: string;
-    constructor(status: number, messsage: string, pathFile: string) {
-      super(messsage);
-      this.status = status;
-      this.pathFile = pathFile;
-    }
+  status: number;
+  pathFile: string;
+  constructor(status: number, messsage: string, pathFile: string) {
+    super(messsage);
+    this.status = status;
+    this.pathFile = pathFile;
   }
-  
-  export { VerificationResponseError };
-  
+}
+
+export { VerificationResponseError };
