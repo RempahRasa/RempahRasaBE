63de66acdf78b8082186475590a58b799dc054a5
Daffadon <daffaputranarendra9@gmail.com>
2024-06-05 13:58:59 +0700

    fix: change auth middleware placement to fix not found route

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,13 +1,21 @@
+import { ResponseError } from '../error/response-error';
 import { MiddlewareRequest } from '../interface/controller';
+import { authMiddleware } from '../middleware/auth-middleware';
 import { getHistoryService } from '../service/user/profile/history-service';
 import { getProfileService } from '../service/user/profile/profile-service';
 
 const getProfileController = async ({ req, res, next }: MiddlewareRequest) => {
   try {
+    const checkAuth = await authMiddleware(req, res, next);
+    if (!checkAuth) {
+      throw new ResponseError(401, 'Unauthorized');
+    }
     const result = await getProfileService(req, res);
-    res.status(200).json({
-      data: result
-    });
+    if (result) {
+      res.status(200).json({
+        data: result
+      });
+    }
   } catch (error) {
     next(error);
   }
@@ -15,6 +23,10 @@ const getProfileController = async ({ req, res, next }: MiddlewareRequest) => {
 
 const getHistoriesController = async ({ req, res, next }: MiddlewareRequest) => {
   try {
+    const checkAuth = await authMiddleware(req, res, next);
+    if (!checkAuth) {
+      throw new ResponseError(401, 'Unauthorized');
+    }
     const result = await getHistoryService(req, res);
     res.status(200).json({
       data: result
