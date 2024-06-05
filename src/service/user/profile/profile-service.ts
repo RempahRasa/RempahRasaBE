6bc3177d764be7b81ecaad63367980a9a96cc5d2
Daffadon <daffaputranarendra9@gmail.com>
2024-06-05 14:00:53 +0700

    style: change code placement and remove unused code

--- src/service/user/profile/profile-service.ts
+++ src/service/user/profile/profile-service.ts
@@ -6,16 +6,16 @@ import { getTokenFromHeader } from '../../../utils/token';
 const getProfileService = async (req: Request, res: Response) => {
   const accessToken = getTokenFromHeader(req, res);
   const { userData } = await getUserByToken(accessToken);
-  if (userData) {
-    return {
-      firstName: userData.firstName,
-      lastName: userData.lastName,
-      email: userData.email,
-      image: userData.image
-    };
-  } else {
+
+  if (!userData) {
     throw new ResponseError(404, 'User not found');
   }
+  return {
+    firstName: userData.firstName,
+    lastName: userData.lastName,
+    email: userData.email,
+    image: userData.image
+  };
 };
 
 export { getProfileService };
