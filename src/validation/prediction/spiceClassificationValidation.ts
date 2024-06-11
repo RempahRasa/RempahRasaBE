bb5b66f2defa06c2d4a1c596c126edfe00b4c937
Daffadon <daffaputranarendra9@gmail.com>
2024-06-11 23:25:57 +0700

    feat: add create spice history and the validation

---
+++ src/validation/prediction/spiceClassificationValidation.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+
+const FILESIZE = 5000000;
+const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png'];
+
+export const spiceClassificationValidation = z
+  .any()
+  .optional()
+  .refine(
+    (file) => {
+      if (!file) {
+        return false;
+      }
+      if (file.size >= FILESIZE) {
+        return false;
+      }
+      if (!ACCEPTED_IMAGE_TYPES.includes(file.mimetype)) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: 'file size over 5mb or only jpeg, jpg, png, allowed'
+    }
+  );
