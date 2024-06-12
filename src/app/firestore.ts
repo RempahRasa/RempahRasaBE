35ad1cb6d6a0b5a50cb0013ff582d0006e60370a
Reqyan <reqyan.1103@gmail.com>
2024-06-12 20:42:02 +0700

    feat: add favorites feature

--- src/app/firestore.ts
+++ src/app/firestore.ts
@@ -7,6 +7,6 @@ export const db = new Firestore({
 });
 
 export const userCollection = db.collection('users');
-export const historyCollection = async (userId: string) => {
-  return await userCollection.doc(userId).collection('history');
+export const historyCollection =  (userId: string) => {
+  return userCollection.doc(userId).collection('history');
 };
